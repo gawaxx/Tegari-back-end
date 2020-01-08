e8f9991f3e75abec253424bed705372c05f0a3ca
gawaxx <jules.blanc@unccub.com>
2020-01-08 16:07:01 +0000

    Generating seed data for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Destroying all Users in Data Base"
+User.destroy_all
+puts "All users destroyed"
+
+puts "Destroying all Post in Data Base"
+Post.destroy_all 
+puts "All post destroyed"
+
+puts "Destroying all Admin data"
+Admin.destroy_all
+puts "All admin destroyed"
+
+puts "Destroying all Save Post data"
+SavePost.destroy_all
+puts "All SavePost data destroyed"
+
+puts "Generating seed data..."
+
+iuser = 0
+
+while iuser < 11 do
+    User.create(points: 0, name: Faker::Name.first_name, email: Faker::Internet.email, password_digest: "hello", user_name: Faker::Name.middle_name, surname: Faker::Name.last_name, n_of_reports: 0)
+    iuser += 1
+end
+
+puts "Created 10 users"
