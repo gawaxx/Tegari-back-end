4393e9bc7dea23eeb48c3786b53823dc91a0898d
gawaxx <jules.blanc@unccub.com>
2020-01-08 16:59:18 +0000

    Creating views for API

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+    def index
+        posts = Post.all
+        render json: posts
+    end 
+
+    def show 
+        posts = Post.find_by(id: params[:id])
+        render json: posts
+    end
 end
