6fdf633e0bffc0aa0ddcfed779b9d1353cc2b45d
gawaxx <jules.blanc@unccub.com>
2020-01-08 16:18:31 +0000

    Generated seed data for post and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,17 +19,17 @@ puts "All SavePost data destroyed"
 puts "Generating seed data..."
 
 iuser = 0
-
 while iuser < 11 do
     User.create(points: 0, name: Faker::Name.first_name, email: Faker::Internet.email, password_digest: "hello", user_name: Faker::Name.middle_name, surname: Faker::Name.last_name, n_of_reports: 0)
+    puts "Created user number #{iuser}"
     iuser += 1
 end
+puts "Created 10 users"
 
 ipost = 0
-
-while ipost < 11 do
-    Post.create(user_id: Faker::Number.decimal_part(digits: 1), cate: Faker::Name.first_name, email: Faker::Internet.email, password_digest: "hello", user_name: Faker::Name.middle_name, surname: Faker::Name.last_name, n_of_reports: 0)
+while ipost < 25 do
+    Post.create(user_id: Faker::Number.decimal_part(digits: 1), category: Faker::Science.element, price: Faker::Number.decimal_part(digits: 3), postcode: Faker::Address.postcode, address: Faker::Address.street_name, city: Faker::Address.city, description: Faker::Hacker.say_something_smart, image_url: "0", condition: "new")
+    puts "Created post number #{ipost}"
     ipost += 1
 end
-
-puts "Created 10 users"
+puts "Created 25 posts"
