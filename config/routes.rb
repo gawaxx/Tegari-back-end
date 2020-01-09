6a270df2a9fc7fbac95d70941c0602af05e836cf
gawaxx <jules.blanc@unccub.com>
2020-01-09 17:06:42 +0000

    Adding Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :admins
   resources :posts
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Custom Routes
+  post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
+
 end
