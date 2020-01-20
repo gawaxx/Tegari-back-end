4095d77ef04394bc09ac83a23ad2e10e0c3c6a08
gawaxx <jules.blanc@unccub.com>
2020-01-20 10:45:41 +0000

    Creating post

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,4 +10,9 @@ class ReportsController < ApplicationController
         render json: report
     end
 
+    def create
+        report = Report.create(user_reported_id: params[:user_reported_id], user_reporting_id: params[:user_reporting_id], reason_reporting: params[:reason_reporting], post_id_reported: params[:post_id_reported])
+        render json: report
+    end 
+
 end
