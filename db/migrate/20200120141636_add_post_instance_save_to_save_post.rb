07b88a8dbf8badf1715406267745e4daaf3fadd0
gawaxx <jules.blanc@unccub.com>
2020-01-20 14:17:04 +0000

    More migration

--- db/migrate/20200120141636_add_post_instance_save_to_save_post.rb
+++ db/migrate/20200120141636_add_post_instance_save_to_save_post.rb
@@ -1,4 +1,4 @@
-class AddPostSaveToSavePost < ActiveRecord::Migration[6.0]
+class AddPostInstanceSaveToSavePost < ActiveRecord::Migration[6.0]
   def change
     add_column :save_posts, :post, :text, array: true
   end
